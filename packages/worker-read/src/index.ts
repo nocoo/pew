a6216d9765927eae25f9027b97e019ea72c91e15
Zheng Li <lizheng@lizheng.me>
2026-03-26 13:11:52 +0800

    chore: bump version to 1.14.7

--- packages/worker-read/src/index.ts
+++ packages/worker-read/src/index.ts
@@ -19,7 +19,7 @@
 // Version
 // ---------------------------------------------------------------------------
 
-export const WORKER_VERSION = "1.14.6";
+export const WORKER_VERSION = "1.14.7";
 
 // ---------------------------------------------------------------------------
 // Boot timestamp (for uptime calculation)
