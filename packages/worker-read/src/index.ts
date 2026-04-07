07be3f97fb2d63bbb99903062bdcfbd488647bee
Zheng Li <lizheng@lizheng.me>
2026-04-07 16:10:26 +0800

    chore: bump version to 2.2.9

--- packages/worker-read/src/index.ts
+++ packages/worker-read/src/index.ts
@@ -19,7 +19,7 @@
 // Version
 // ---------------------------------------------------------------------------
 
-export const WORKER_VERSION = "2.2.8";
+export const WORKER_VERSION = "2.2.9";
 
 // ---------------------------------------------------------------------------
 // Boot timestamp (for uptime calculation)
